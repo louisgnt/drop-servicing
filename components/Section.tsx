af33a94bdab4c9979bf70452d375277423d50bb8
.mxw <gangnant.louis@gmail.com>
2024-12-29 21:30:15 +0100

    first commit

--- components/Section.tsx
+++ components/Section.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from "react";
-import { cn } from "@/app/lib/utils";
+import { cn } from "@/lib/utils";
 
 export default function Section(
   props: PropsWithChildren<{ className?: string }>
